1ff23fddc285670e4c17fdaaaf8038d182b2cbf9
vanessa <mromerovanessa.soy@gmail.com>
2021-07-20 00:56:54 -0500

    Changes in main theme

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -1,21 +1,20 @@
-
-import { red } from '@material-ui/core/colors';
-import createTheme from '@material-ui/core/styles/createTheme';
+import { red } from "@material-ui/core/colors";
+import createTheme from "@material-ui/core/styles/createTheme";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#ff0000",
     },
     secondary: {
-      main: '#19857b',
+      main: "#19857b",
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: "#fff",
     },
   },
 });
